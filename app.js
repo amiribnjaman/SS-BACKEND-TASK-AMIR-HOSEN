86955698a4c45c6931eeba84d56dafb9a813f8c6
amiribnjaman <amiribnjaman@gmail.com>
2023-06-21 14:36:34 +0600

    Basic error set

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
+// Handling url error
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Bad request'
+    })
+})
+
+// Handling server error
+app.use((error, req, res, next) => {
+    res.status(500).json({
+        message: "Internal problem occur"
+    })
+})
+
 
 // Testing get route
 app.get('/', (req, res) => {
